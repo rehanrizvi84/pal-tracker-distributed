1261535ee5f74ebdc9e078c55913252d182b4ffc
rehanrizvi84 <gopinath.miryala@gmail.com>
2018-07-12 10:31:11 -0600

    test commit

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -17,6 +17,7 @@ public class App {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(App.class, args);
+        System.out.println("hello");
     }
 
     @Bean
